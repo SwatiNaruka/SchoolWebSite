e139605be9cf118ed45870d61b5ce5b81a3b7a81
SwatiNaruka <swatijadon91@gmail.com>
2021-08-24 21:37:39 +0545

    My first Commit

---
+++ src/main/java/servlet/login.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class login extends HttpServlet{
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			
+			 String firstName = req.getParameter("email");
+			 System.out.println("firstName");
+		}
+		
+		
+}
---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,305 @@
+$(document).ready(function(){
+	  $('#Learning-Material').hide();
+	     	$('#Admission-form').hide();
+			$('#job').hide();
+			
+	    
+	    
+ });
+
+$(document).ready(function(){
+	  $('#learn').click(function(){
+				$('#Learning-Material').show();		
+	     	$('#Admission-form').hide();
+			$('#job').hide();
+			
+	    
+	    });
+ });
+
+
+$(document).ready(function(){
+	  $('#admmision').click(function(){	
+			$('#Admission-form').show();		
+	     	$('#Learning-Material').hide();			
+			$('#job').hide();
+			
+	    
+	    });
+ });
+
+$(document).ready(function(){
+	  $('#job-form').click(function(){
+		$('#job').show();
+	     	$('#Learning-Material').hide();
+			
+			$('#Admission-form').hide();
+			
+	    
+	    });
+ });
+
+/*----------------------------------------Faculities------------------------------*/
+
+
+$(document).ready(function(){
+	  
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').hide();
+			$('#lab-detail').hide();
+			$('#transport-detail').hide();
+			$('#Sports-detail').hide();
+
+ });
+
+$(document).ready(function(){
+	  $('#com').click(function(){
+			$('#com-detail').show();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').hide();
+			$('#lab-detail').hide();
+			$('#transport-detail').hide();
+			$('#Sports-detail').hide();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+$(document).ready(function(){
+	  $('#lib').click(function(){
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').show();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').hide();
+			$('#lab-detail').hide();
+			$('#transport-detail').hide();
+			$('#Sports-detail').hide();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+$(document).ready(function(){
+	  $('#security').click(function(){
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').show();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').hide();
+			$('#lab-detail').hide();
+			$('#transport-detail').hide();
+			$('#Sports-detail').hide();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+
+$(document).ready(function(){
+	  $('#cafe').click(function(){
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').show();
+			$('#edu-detail').hide();
+			$('#lab-detail').hide();
+			$('#transport-detail').hide();
+			$('#Sports-detail').hide();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+$(document).ready(function(){
+	  $('#edu').click(function(){
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').show();
+			$('#lab-detail').hide();
+			$('#transport-detail').hide();
+			$('#Sports-detail').hide();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+$(document).ready(function(){
+	  $('#lab').click(function(){
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').hide();
+			$('#lab-detail').show();
+			$('#transport-detail').hide();
+			$('#Sports-detail').hide();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+$(document).ready(function(){
+	  $('#transport').click(function(){
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').hide();
+			$('#lab-detail').hide();
+			$('#transport-detail').show();
+			$('#Sports-detail').hide();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+$(document).ready(function(){
+	  $('#sport').click(function(){
+			$('#com-detail').hide();
+	     	$('#liabrary-detail').hide();
+			$('#Security-detail').hide();			
+			$('#cafe-detail').hide();
+			$('#edu-detail').hide();
+			$('#lab-detail').hide();
+			$('#transport-detail').hide();
+			$('#Sports-detail').show();
+			
+			
+			
+			$('#com').hide();
+	     	$('#lib').hide();
+			$('#security').hide();			
+			$('#cafe').hide();
+			$('#edu').hide();
+			$('#lab').hide();
+			$('#transport').hide();
+			$('#sport').hide();
+			$('#down').hide();
+			
+
+	    
+	    });
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
